44fa5fc314960f8fdbdc0bb39b22d59cf5d76afe
Abhishek Kashyap <abhik5567@gmail.com>
2021-11-22 11:58:51 +0530

    Removed the unwanted lines added for testing

--- theUI_3.py
+++ theUI_3.py
@@ -6,15 +6,18 @@ import threading
 import sys
 import asyncio
 import keyboard
+from random import randint
+import multiprocessing as mp
 
-cluster = pymongo.MongoClient(host="mongodb+srv://deep:1234abcd@cluster.ds3cv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient(
+    host="mongodb+srv://deep:1234abcd@cluster.ds3cv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster['socialmedia']['messages']
 db_log = cluster['socialmedia']['log']
 
 
-
-
 class MainWindow(QMainWindow):
+
+    opened = False
 
     def __init__(self):
         """Opens all necessary windows and buttons as needed """
@@ -27,7 +30,6 @@ class MainWindow(QMainWindow):
         self.loginUI()
         self.startupUI()
 
-    
     def signupUI(self):
         """UI FOR THE SIGN_UP SCREEN"""
         self.signup_bg = QtWidgets.QLabel(self)
@@ -41,7 +43,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.username_signup.setFont(font)
         self.username_signup.setText('*username*')
-        self.username_signup.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.username_signup.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.password_signup = QtWidgets.QLineEdit(self)
         self.password_signup.setGeometry(QtCore.QRect(60, 280, 261, 41))
@@ -49,7 +52,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.password_signup.setFont(font)
         self.password_signup.setText("*password*")
-        self.password_signup.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.password_signup.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.sign_up_btn = QtWidgets.QPushButton(self)
         self.sign_up_btn.setGeometry(QtCore.QRect(80, 340, 220, 41))
@@ -57,17 +61,16 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.sign_up_btn.setFont(font)
         self.sign_up_btn.setText("Sign up for new account")
-        self.sign_up_btn.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.sign_up_btn.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.sign_up_btn.clicked.connect(self.sign_up_func)
-
 
     '''FUNCTIONS FOR THE SIGN_UP SCREEN'''
 
-    
     def sign_up_func(self):
         '''create a user account'''
-        set_log_name =  self.username_signup.text()
-        set_log_pass =  self.password_signup.text()
+        set_log_name = self.username_signup.text()
+        set_log_pass = self.password_signup.text()
         if set_log_name != '*username*':
             self.remove_signupUI()
             credential = {'log_name': set_log_name, 'log_pass': set_log_pass}
@@ -75,14 +78,12 @@ class MainWindow(QMainWindow):
         self.remove_signupUI()
         self.insert_loginUI()
 
-    
     def remove_signupUI(self):
         '''removes the SIGNUP ui'''
         self.signup_bg.setVisible(False)
         self.username_signup.setVisible(False)
         self.password_signup.setVisible(False)
         self.sign_up_btn.setVisible(False)
-
 
     def insert_signupUi(self):
         '''displays the SIGNUP UI'''
@@ -104,7 +105,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.startup_sign_in_button.setFont(font)
         self.startup_sign_in_button.setText('Sign In')
-        self.startup_sign_in_button.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.startup_sign_in_button.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.startup_sign_in_button.clicked.connect(self.startup_sign_in_func)
 
         self.startup_sign_up_button = QtWidgets.QPushButton(self)
@@ -113,20 +115,19 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.startup_sign_up_button.setFont(font)
         self.startup_sign_up_button.setText("Sign Up")
-        self.startup_sign_up_button.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.startup_sign_up_button.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.startup_sign_up_button.clicked.connect(self.startup_sign_up_func)
 
     '''FUNCTUONS FOR THE STARTUP SCREEN'''
-
 
     def startup_sign_in_func(self):
         '''takes to sign in screen'''
         self.startup_sign_up_button.setVisible(False)
         self.startup_sign_in_button.setVisible(False)
         self.startup_bg.setVisible(False)
-        #del(self.sign_up_ui)
+        # del(self.sign_up_ui)
         self.remove_signupUI()
-
 
     def startup_sign_up_func(self):
         ''' take to sign up screen'''
@@ -135,9 +136,6 @@ class MainWindow(QMainWindow):
         self.startup_bg.setVisible(False)
         self.remove_loginUI()
         self.insert_signupUi()
-
-
-
 
     def loginUI(self):
         '''UI FOR THE LOGIN SCREEN'''
@@ -152,7 +150,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.username_login.setFont(font)
         self.username_login.setText('*username*')
-        self.username_login.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.username_login.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.password_login = QtWidgets.QLineEdit(self)
         self.password_login.setGeometry(QtCore.QRect(60, 280, 261, 41))
@@ -160,7 +159,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.password_login.setFont(font)
         self.password_login.setText("*password*")
-        self.password_login.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.password_login.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.sign_in_btn = QtWidgets.QPushButton(self)
         self.sign_in_btn.setGeometry(QtCore.QRect(120, 340, 131, 41))
@@ -168,20 +168,20 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.sign_in_btn.setFont(font)
         self.sign_in_btn.setText("Sign In")
-        self.sign_in_btn.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.sign_in_btn.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.sign_in_btn.clicked.connect(self.sign_in_func)
 
         self.wrong_pass = QtWidgets.QLineEdit(self)
         self.wrong_pass.setGeometry(QtCore.QRect(110, 180, 150, 31))
         self.wrong_pass.setText("[Name/Pass didnt matched]")
         self.wrong_pass.setReadOnly(True)
-        self.wrong_pass.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.wrong_pass.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.wrong_pass.setVisible(False)
-
 
     '''FUNCTIONS FOR THE LOGIN SCREEN'''
 
-    
     def sign_in_func(self):
         ''' logs in the user'''
         log_name = self.username_login.text()
@@ -192,7 +192,6 @@ class MainWindow(QMainWindow):
         else:
             self.wrong_pass.setVisible(True)
 
-    
     def remove_loginUI(self):
         '''removes the login ui'''
         self.login_bg.setVisible(False)
@@ -200,8 +199,8 @@ class MainWindow(QMainWindow):
         self.username_login.setVisible(False)
         self.sign_in_btn.setVisible(False)
         self.wrong_pass.setVisible(False)
+        self.opened = True
 
-    
     def insert_loginUI(self):
         '''displays the login ui'''
         self.login_bg.setVisible(True)
@@ -209,9 +208,6 @@ class MainWindow(QMainWindow):
         self.username_login.setVisible(True)
         self.sign_in_btn.setVisible(True)
 
-
-
-    
     def initUI(self):
         ''' UI FOR THE MAIN SCREEN'''
 
@@ -220,7 +216,8 @@ class MainWindow(QMainWindow):
         font = QtGui.QFont()
         font.setPointSize(14)
         self.text_box.setFont(font)
-        self.text_box.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.text_box.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
         self.text_box.returnPressed.connect(self.update_db)
 
         self.send_btn = QtWidgets.QPushButton(self)
@@ -230,7 +227,8 @@ class MainWindow(QMainWindow):
         self.send_btn.setFont(font)
         self.send_btn.setText("SEND")
         self.send_btn.clicked.connect(self.update_db)
-        self.send_btn.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.send_btn.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.msg_box = QtWidgets.QTextEdit(self)
         self.msg_box.setGeometry(QtCore.QRect(30, 50, 311, 451))
@@ -238,7 +236,8 @@ class MainWindow(QMainWindow):
         font.setPointSize(12)
         self.msg_box.setFont(font)
         self.msg_box.setReadOnly(True)
-        self.msg_box.setStyleSheet("background-color: rgb(100, 100, 100); color: rgb(255, 255, 255)")
+        self.msg_box.setStyleSheet(
+            "background-color: rgb(100, 100, 100); color: rgb(255, 255, 255)")
 
         self.bg = QtWidgets.QLabel(self)
         self.bg.setGeometry(QtCore.QRect(10, 10, 351, 601))
@@ -248,48 +247,54 @@ class MainWindow(QMainWindow):
         self.bg.lower()
 
         self.refresh_btn = QtWidgets.QPushButton(self)
-        self.refresh_btn.setGeometry(280,15,50,30)
+        self.refresh_btn.setGeometry(280, 15, 50, 30)
         self.refresh_btn.setText('Refresh')
         self.refresh_btn.clicked.connect(self.refresh)
-        self.refresh_btn.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.refresh_btn.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.delete_all_btn = QtWidgets.QPushButton(self)
         self.delete_all_btn.setGeometry(30, 15, 70, 30)
         self.delete_all_btn.setText('Delete All')
         self.delete_all_btn.clicked.connect(self.delete_all)
-        self.delete_all_btn.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.delete_all_btn.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
         self.new_msg = QtWidgets.QLineEdit(self)
-        self.new_msg.setGeometry(120,15,150,30)
+        self.new_msg.setGeometry(120, 15, 150, 30)
         self.new_msg.setReadOnly(True)
         self.new_msg.setText('No New Messages')
-        self.new_msg.setStyleSheet("color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
+        self.new_msg.setStyleSheet(
+            "color:rgb(255, 255, 255); background-color: rgb(100, 100, 100)")
 
-        t = threading.Thread(target=self.update_op)
-        t.start()
+        ctx = mp.get_context('spawn')
+        p = ctx.Process(target=self.update_op)
+        p.start()
 
     '''FUNCTIONS FOR THE MAIN SCREEN'''
 
-    
     def new_msg_check(self):
         '''checks for new messages, and displays alert on the screen'''
         print("123")
         '''while True:
             self.update_op()'''
 
-    
     def refresh(self):
         '''refreshes new messages'''
-        #self.update_op()
+        # self.update_op()
         self.new_msg.setText('No New Messages')
 
-    
+        def get_messages() -> list:
+            """gets all the messages from the server """
+            for messages in db.find({}):
+                yield f"[{messages['alias']}]: {messages['message']}"
+        self.msg_box.setText("\n".join(get_messages()))
+
     def delete_all(self):
         '''deletes all the messages'''
         db.delete_many({})
         self.msg_box.setText('')
 
-    
     def update_db(self):
         '''inserts the new text on the database'''
         self.new_msg.setText('No New Messages')
@@ -299,23 +304,20 @@ class MainWindow(QMainWindow):
         db.insert_one(msg)
         self.text_box.clear()
 
-    
     def update_op(self):
         '''updates the messages in the screen'''
-        print(dir(self.msg_box))
-        while self.msg_box.isHidden:
-            pass
+        while not self.opened:
+            print(str(randint(1, 10000)))
+        self.msg_box.setText(str(randint(1, 10000)))
         while True:
-            def get_messages():
+            def get_messages() -> list:
                 """gets all the messages from the server """
                 for messages in db.find({}):
                     yield f"[{messages['alias']}]: {messages['message']}"
-            #self.msg_box.setText("\n".join(list(get_messages())))
-            #print("lol")
-            #print("\n".join(get_messages()))
-    
-    
+            # self.msg_box.setText("\n".join(list(get_messages())))
 
+            # print("lol")
+            # print("\n".join(get_messages()))
 
 
 if __name__ == "__main__":
